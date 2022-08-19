4ba8add73976b495a2fa6441832d4df5626d7250
Adela <94903075+adela-bytebase@users.noreply.github.com>
2022-08-19 11:05:15 +0800

    [Bytebase] Update latest schema for "employeeGitHub" after migration 202208181600
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    https://devrel-bytebase-demo.onrender.com:443/issue/drop-nickname-by-test-employeegithub__202208181600__migrate__drop_nickname-sql-119?stage=128
    
    --------Original migration change--------
    
    https://github.com/adela-bytebase/bytebase-gitops-sample/commit/8e9de953bae915b54eeb945f3539aa4f3095befc
    
    Merge pull request #7 from adela-bytebase/test-them
    
    test drop

--- bytebase/test/.employeeGitHub__LATEST.sql
+++ bytebase/test/.employeeGitHub__LATEST.sql
@@ -56,7 +56,6 @@ CREATE TABLE `employee` (
   `last_name` varchar(16) NOT NULL,
   `gender` enum('M','F') NOT NULL,
   `hire_date` date NOT NULL,
-  `nickname` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`emp_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
